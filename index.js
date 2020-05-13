38f4d2dc42269f4bc9b71beaf5120705a28617e1
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-13 10:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 const names=[['Ada','Brendan','Ali'],'birthday'];
 writeCards (names[0],names[1]);
-Function writeCards(names,eventName){
+function writeCards(names,eventName){
   for(let i=0;i<names.length;i++)
   {
     console.log(`Thank you ${names[i]} for the wonderful ${eventName} gift!`);
